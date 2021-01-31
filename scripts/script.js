69fc7f4b400b2c9d46a6b27bc234e96270f2539f
Riley Chou <riley33chou@gmail.com>
2021-01-30 16:38:53 -0800

    links

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,15 @@ const secretDiv = document.querySelector('.secret-div');
 const keys = [];
 const secretCode = ['i','c','e','s','h','a','r','k'];
 
-const confettiSettings = { target: 'my-canvas', size: 1.4, max: 150};
-const confetti = new ConfettiGenerator(confettiSettings);
+//const confettiSettings = { target: 'my-canvas', size: 1.4, max: 150};
+//const confetti = new ConfettiGenerator(confettiSettings);
 
 function keysDetector(e) {
     keys.push(e.key);
 
     if (JSON.stringify(keys) === JSON.stringify(secretCode)){
         secretDiv.style.display = 'block';
-        confetti.render();
+        //confetti.render();
 
         anime({
             targets:'body',
