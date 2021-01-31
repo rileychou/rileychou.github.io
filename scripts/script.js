eb30e2b0853073215669bf230aac62870d18b1ff
Riley Chou <riley33chou@gmail.com>
2021-01-30 16:30:43 -0800

    chicken nuggo

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+const secretDiv = document.querySelector('.secret-div');
+
+const keys = [];
+const secretCode = ['i','c','e','s','h','a','r','k'];
+
+const confettiSettings = { target: 'my-canvas', size: 1.4, max: 150};
+const confetti = new ConfettiGenerator(confettiSettings);
+
+function keysDetector(e) {
+    keys.push(e.key);
+
+    if (JSON.stringify(keys) === JSON.stringify(secretCode)){
+        secretDiv.style.display = 'block';
+        confetti.render();
+
+        anime({
+            targets:'body',
+            rotate: '1turn',
+            backgroundColor: '#fcba03',
+            duration: 2000,
+        }) 
+    }
+    if (keys.length > secretCode.length){
+        keys.shift();
+    }
+}
+
+window.addEventListener('keyup', keysDetector);
