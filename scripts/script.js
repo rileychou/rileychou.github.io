de147806b3a3300ab1824eec8df6178e9e78ba74
Riley Chou <riley33chou@gmail.com>
2021-02-02 18:06:08 -0800

    button back

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,10 @@
 const secretDiv = document.querySelector('.secret-div');
 const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+const backButton = document.getElementById('back-button');
 
 const keys = [];
 const secretCode = ['i','c','e','s','h','a','r','k'];
-
-//const confettiSettings = { target: 'my-canvas', size: 1.4, max: 150};
-//const confetti = new ConfettiGenerator(confettiSettings);
 
 function keysDetector(e) {
     keys.push(e.key);
@@ -16,15 +15,23 @@ function keysDetector(e) {
     if (JSON.stringify(keys) === JSON.stringify(secretCode)){
         secretDiv.style.display = 'block';
         main.style.display = 'none';
+        footer.style.display = 'none';
         //confetti.render();
-
         anime({
             targets:'.secret-div',
             rotate: '1turn',
             backgroundColor: '',
-            duration: 2000,
-        }) 
+            duration: 2000
+        });
     }
 }
 
+function goBack() {
+    main.style.display = 'block';
+    main.scrollIntoView();
+    footer.style.display = 'block';
+    setTimeout (() => {secretDiv.style.display = 'none';}, 450);
+}
+
 window.addEventListener('keyup', keysDetector);
+backButton.addEventListener('click', goBack);
