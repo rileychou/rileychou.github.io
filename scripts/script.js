aa7c36a3c9dc0f8b2f1b1af97d31a4ba84881196
Riley Chou <riley33chou@gmail.com>
2021-02-02 20:50:11 -0800

    fixed the god damn animation finally

--- scripts/script.js
+++ scripts/script.js
@@ -4,25 +4,26 @@ const footer = document.querySelector('footer');
 const backButton = document.getElementById('back-button');
 
 const keys = [];
-const secretCode = ['i','c','e','s','h','a','r','k'];
+const secretCode = ['i', 'c', 'e', 's', 'h', 'a', 'r', 'k'];
+
+let spinAnimation = anime({
+    targets: '.secret-div',
+    rotate: '1turn',
+    backgroundColor: '',
+    duration: 2000
+});
 
 function keysDetector(e) {
     keys.push(e.key);
-    if (keys.length > secretCode.length){
+    if (keys.length > secretCode.length) {
         keys.shift();
         console.log(keys);
     }
-    if (JSON.stringify(keys) === JSON.stringify(secretCode)){
-        secretDiv.style.display = 'block';
+    if (JSON.stringify(keys) === JSON.stringify(secretCode)) {
         main.style.display = 'none';
         footer.style.display = 'none';
-        //confetti.render();
-        anime({
-            targets:'.secret-div',
-            rotate: '1turn',
-            backgroundColor: '',
-            duration: 2000
-        });
+        secretDiv.style.display = 'block';
+        spinAnimation.restart();
     }
 }
 
@@ -30,7 +31,7 @@ function goBack() {
     main.style.display = 'block';
     main.scrollIntoView();
     footer.style.display = 'block';
-    setTimeout (() => {secretDiv.style.display = 'none';}, 450);
+    setTimeout(() => { secretDiv.style.display = 'none'; }, 450);
 }
 
 window.addEventListener('keyup', keysDetector);
